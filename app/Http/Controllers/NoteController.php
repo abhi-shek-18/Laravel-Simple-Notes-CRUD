0f3d3f6ba3e4998a50918651b6da4247ae1e541a
abhi-shek-18 <113258421+abhi-shek-18@users.noreply.github.com>
2024-09-20 17:13:49 +0530

    Add files via upload

---
+++ app/Http/Controllers/NoteController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Note;
+use Illuminate\Http\Request;
+use App\Http\Requests\NoteStoreRequest;
+use App\Http\Requests\NoteUpdateRequest;
+
+class NoteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $notes=Note::latest()->paginate(5);
+
+        return view('notes.index',compact('notes'))
+        ->with('i',(request()->input('page',-1)-1)*5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('notes.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(NoteStoreRequest  $request)
+    {
+        Note::create($request->validated());
+
+        return redirect()->route('notes.index')
+                            ->with('success',"Note created Successfully");
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Note $note)
+    {
+        return view('notes.show')->with(compact('note'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Note $note)
+    {
+        return view('notes.edit',compact('note'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(NoteUpdateRequest $request, Note $note)
+    {
+        $note->update($request->validated());
+
+        return redirect()->route('notes.index')
+        ->with('success',"Note Updated Successfully"); 
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Note $note)
+    {
+        $note->delete();
+
+        return redirect()->route('notes.index')
+        ->with('success',"Note deleted Successfully");
+    }
+}
